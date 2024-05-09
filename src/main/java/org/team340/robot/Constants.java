980fba999d49a14d32500e69f8f975726a660888
Bryce Roethel <github.vwkbi@8alias.com>
2024-05-08 22:04:27 -0400

    Initial commit

---
+++ src/main/java/org/team340/robot/Constants.java
@@ -0,0 +1,130 @@
+package org.team340.robot;
+
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.wpilibj.ADIS16470_IMU.CalibrationTime;
+import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
+import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import org.team340.lib.controller.Controller2Config;
+import org.team340.lib.swerve.config.SwerveConfig;
+import org.team340.lib.swerve.config.SwerveModuleConfig;
+import org.team340.lib.swerve.hardware.motors.SwerveMotor;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ */
+public final class Constants {
+
+    public static final double PERIOD = 0.020;
+    public static final double TELEMETRY_PERIOD = 0.020;
+    public static final double POWER_USAGE_PERIOD = 0.020;
+    public static final double VOLTAGE = 12.0;
+    public static final double FIELD_LENGTH = 16.541;
+    public static final double FIELD_WIDTH = 8.211;
+
+    /**
+     * Driver and co-driver controller constants.
+     */
+    public static final class ControllerConstants {
+
+        public static final double DRIVE_EXP = 1.0;
+        public static final double DRIVE_MULTIPLIER = 0.9;
+        public static final double DRIVE_MULTIPLIER_MODIFIED = 1.0;
+
+        public static final double DRIVE_ROT_EXP = 2.0;
+        public static final double DRIVE_ROT_MULTIPLIER = 0.4;
+
+        public static final Controller2Config DRIVER = new Controller2Config()
+            .setLabel("Driver")
+            .setPort(0)
+            .setJoystickDeadband(0.15)
+            .setJoystickThreshold(0.7)
+            .setTriggerDeadband(0.1)
+            .setTriggerThreshold(0.1);
+
+        public static final Controller2Config CO_DRIVER = new Controller2Config()
+            .setLabel("CoDriver")
+            .setPort(1)
+            .setJoystickDeadband(0.15)
+            .setJoystickThreshold(0.7)
+            .setTriggerDeadband(0.1)
+            .setTriggerThreshold(0.1);
+    }
+
+    /**
+     * Map of hardware device IDs.
+     */
+    public static final class RobotMap {
+
+        public static final int FRONT_LEFT_MOVE = 2;
+        public static final int FRONT_LEFT_TURN = 3;
+        public static final int BACK_LEFT_MOVE = 4;
+        public static final int BACK_LEFT_TURN = 5;
+        public static final int BACK_RIGHT_MOVE = 6;
+        public static final int BACK_RIGHT_TURN = 7;
+        public static final int FRONT_RIGHT_MOVE = 8;
+        public static final int FRONT_RIGHT_TURN = 9;
+    }
+
+    /**
+     * Constants for the swerve subsystem.
+     */
+    public static final class SwerveConstants {
+
+        private static final SwerveModuleConfig FRONT_LEFT = new SwerveModuleConfig()
+            .setLabel("Front Left")
+            .useSparkAttachedEncoder(2.389, true)
+            .setPosition(0.288925, 0.288925)
+            .setMoveMotor(RobotMap.FRONT_LEFT_MOVE, true, true)
+            .setTurnMotor(RobotMap.FRONT_LEFT_TURN, false, true);
+
+        private static final SwerveModuleConfig BACK_LEFT = new SwerveModuleConfig()
+            .setLabel("Back Left")
+            .useSparkAttachedEncoder(1.999, true)
+            .setPosition(-0.288925, 0.288925)
+            .setMoveMotor(RobotMap.BACK_LEFT_MOVE, true, true)
+            .setTurnMotor(RobotMap.BACK_LEFT_TURN, false, true);
+
+        private static final SwerveModuleConfig BACK_RIGHT = new SwerveModuleConfig()
+            .setLabel("Back Right")
+            .useSparkAttachedEncoder(4.194, true)
+            .setPosition(-0.288925, -0.288925)
+            .setMoveMotor(RobotMap.BACK_RIGHT_MOVE, true, true)
+            .setTurnMotor(RobotMap.BACK_RIGHT_TURN, false, true);
+
+        private static final SwerveModuleConfig FRONT_RIGHT = new SwerveModuleConfig()
+            .setLabel("Front Right")
+            .useSparkAttachedEncoder(5.566, true)
+            .setPosition(0.288925, -0.288925)
+            .setMoveMotor(RobotMap.FRONT_RIGHT_MOVE, true, true)
+            .setTurnMotor(RobotMap.FRONT_RIGHT_TURN, false, true);
+
+        public static final SwerveConfig CONFIG = new SwerveConfig()
+            .useADIS16470(IMUAxis.kZ, IMUAxis.kX, IMUAxis.kY, Port.kOnboardCS0, CalibrationTime._4s)
+            .setPeriod(PERIOD)
+            .setMovePID(0.3, 0.0, 0.0, 0.0)
+            .setMoveFF(0.1, 2.5, 0.0)
+            .setTurnPID(0.65, 0.001, 3.0, 0.01)
+            .setRampRate(0.03, 0.03)
+            .setMotorTypes(SwerveMotor.Type.SPARK_FLEX_BRUSHLESS, SwerveMotor.Type.SPARK_FLEX_BRUSHLESS)
+            .setMaxSpeeds(5.0, 12.0)
+            .setRatelimits(11.0, 30.0)
+            .setTrajectoryConstraints(4.0, 8.0)
+            .setPowerProperties(VOLTAGE, 80.0, 40.0)
+            .setMechanicalProperties(6.75, 150.0 / 7.0, 4.0)
+            .setDiscretizationLookahead(PERIOD)
+            .setOdometryPeriod(PERIOD)
+            .setOdometryStd(0.003, 0.003, 0.0012)
+            .setVisionStd(0.0, 0.0, 0.0)
+            .setSysIdConfig(new SysIdRoutine.Config(Volts.of(1.0).per(Seconds.of(0.4)), Volts.of(7.0), Seconds.of(5.5)))
+            .setFieldSize(FIELD_LENGTH, FIELD_WIDTH)
+            .addModule(FRONT_LEFT)
+            .addModule(BACK_LEFT)
+            .addModule(BACK_RIGHT)
+            .addModule(FRONT_RIGHT);
+    }
+}
---
+++ src/main/java/org/team340/robot/Robot.java
@@ -0,0 +1,76 @@
+package org.team340.robot;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.team340.lib.GRRDashboard;
+import org.team340.lib.util.config.rev.RevConfigRegistry;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public final class Robot extends TimedRobot {
+
+    public Robot() {
+        super(Constants.PERIOD);
+    }
+
+    @Override
+    public void robotInit() {
+        LiveWindow.setEnabled(false);
+        enableLiveWindowInTest(false);
+        LiveWindow.disableAllTelemetry();
+        DriverStation.silenceJoystickConnectionWarning(true);
+
+        DataLogManager.start();
+        DataLogManager.logNetworkTables(true);
+        DriverStation.startDataLog(DataLogManager.getLog());
+        GRRDashboard.initSync(this, Constants.TELEMETRY_PERIOD, Constants.POWER_USAGE_PERIOD);
+        RevConfigRegistry.init(this);
+        RobotContainer.init();
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {}
+
+    @Override
+    public void autonomousInit() {
+        GRRDashboard.getAutoCommand().schedule();
+    }
+
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void teleopInit() {}
+
+    @Override
+    public void teleopPeriodic() {}
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
+}
